104574b1be82b10da7bf5ae1ecbb3ba11c97285a
wei-paul <paul.wei607@gmail.com>
2021-10-16 20:44:46 +0000

    database

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -4,5 +4,15 @@ DROP TABLE IF EXISTS widgets CASCADE;
 CREATE TABLE widgets (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL
+  pins_id INTEGER REFERENCES pins(id)
+);
+
+DROP TABLE IF EXISTS pins CASCADE;
+CREATE TABLE pins (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  description VARCHAR(255),
+  coordinates VARCHAR(255)
 );
