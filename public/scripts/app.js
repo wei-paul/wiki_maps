46397d83091af034acb2461f42e87bc51ac0ad93
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-18 13:47:34 -0700

    implement popup inside createmap

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,8 @@
 // Client facing scripts here
 let map2, newMarker, markerLocation;
-// let markerButton = false;
 
 $(document).ready(function() {
+  let markerButton = false;
   var vancouver = L.map('map2').setView([49.2827, -123.1207], 10);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -15,20 +15,34 @@ $(document).ready(function() {
   }).addTo(vancouver);
 
 
-  // vancouver.on('click', addMarker);
-  // function addMarker(temp) {
-  //   if (markerButton) {
-  //     let newMarker = new L.marker(temp.latlng).addTo(vancouver);
-  //     markerButton = false;
-  //   }
-  // }
+  vancouver.on('click', addMarker);
+  function addMarker(temp) {
+    if (markerButton) {
+      let newMarker = new L.marker(temp.latlng).addTo(vancouver);
 
-  // $(".test-button").on("click", () => {
-  //   console.log(markerButton);
-  //   markerButton = true;
-  // });
+      // let tagTitle = prompt("Please enter the title of your marker");
+      $('.center').show();
+      $(this).hide();
 
-  // while (markerButton) {
-  //   cursor
-  // }
+      $('#close').on('click', function () {
+        $('.center').hide();
+      })
+
+      $("#tag-information").submit(function(event) {
+        event.preventDefault();
+        newMarker.bindPopup(`<b>${$(this).find("#Tag-title").val()}</b><img width="150" height="150" src="${$(this).find("#Image-url-link").val()}">`);
+        $('.center').hide();
+      })
+
+      $("#map2").css("cursor", "")
+      markerButton = false;
+
+    }
+  }
+
+  $(".test-button").on("click", () => {
+    $("#map2").css("cursor", "url(https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png) 12 43, move")
+    markerButton = true;
+  });
+
 });
