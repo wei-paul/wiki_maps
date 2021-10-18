05b676abd5eb468f370a7c2b94424792ece5bd5d
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-17 17:19:09 -0700

    added functional scrollbar

--- public/scripts/showcase_scrollbar.js
+++ public/scripts/showcase_scrollbar.js
@@ -1,1 +1,24 @@
 // Client facing scripts here
+const scrollbar = function() {
+$('#scrollButtonRight').click(function(event) {
+  event.preventDefault();
+  $('#showcase').animate({
+    scrollLeft: "+=317ev"
+  }, "fast");
+});
+
+ $('#scrollButtonLeft').click(function(event) {
+  event.preventDefault();
+  $('#showcase').animate({
+    scrollLeft: "-=317ev"
+  }, "fast");
+});
+}
+
+// $('#scrollButtonLeft').on("click", () => {
+//   $('#showcase').scrollLeft()
+// }, "fast");
+
+// $(".snap-left").on("click", () => {
+//   document.documentElement.scrollTop = 0;
+// });
