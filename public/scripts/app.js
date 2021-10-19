163698b69af62d29e2c635dd54678a31650ac12b
KurtSpiker <kurt.spiker@gmail.com>
2021-10-19 22:01:19 +0000

    JSON creation

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 // Client facing scripts here
-let map2, newMarker, markerLocation;
+let map2, markerLocation;
 
 $(document).ready(function() {
   let markerButton = false;
@@ -18,7 +18,22 @@ $(document).ready(function() {
   vancouver.on('click', addMarker);
   function addMarker(temp) {
     if (markerButton) {
-      let newMarker = new L.marker(temp.latlng).addTo(vancouver);
+      let markerInfo = {
+        "type": "Feature",
+        "properties": {
+        "user_id": "Test name",
+        "popupContent": "Test2",
+        "popupimageURL": ""
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-122.3045, 49.0504]
+        }
+      }
+
+      let newMarker = new L.marker(temp.latlng)
+
+      let coordsArray = [temp.latlng.lng, temp.latlng.lat];
 
       // let tagTitle = prompt("Please enter the title of your marker");
       $('.center').show();
@@ -28,15 +43,26 @@ $(document).ready(function() {
         $('.center').hide();
       })
 
+      $("#tag-information").unbind('submit')
       $("#tag-information").submit(function(event) {
         event.preventDefault();
         newMarker.bindPopup(`<b>${$(this).find("#Tag-title").val()}</b><img width="150" height="150" src="${$(this).find("#Image-url-link").val()}">`);
         $('.center').hide();
+
+        let markerProperties = {
+        "user_id": "Test name",
+        "popupContent": `${$(this).find("#Tag-title").val()}`,
+        "popupimageURL": `${$(this).find("#Image-url-link").val()}`
+        }
+        markerInfo.geometry.coordinates = coordsArray;
+        markerInfo.properties = markerProperties;
+
+        console.log(markerInfo) // Marker info updated, ready to append to json
       })
 
       $("#map2").css("cursor", "")
       markerButton = false;
-
+      newMarker.addTo(vancouver);
     }
   }
 
