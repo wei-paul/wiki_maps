893fad0976c135bd8e2e16d059f185a6b6ea49c4
Saam Koukpari <saam.koukpari@gmail.com>
2021-10-18 20:11:54 -0700

    implement button functions

--- public/scripts/createmap.js
+++ public/scripts/createmap.js
@@ -1,31 +1,9 @@
-// $(document).ready(function() {
-//   vancouver.on('click', addMarker);
-//   function addMarker(temp) {
-//     if (markerButton) {
-//       let newMarker = new L.marker(temp.latlng).addTo(vancouver);
+$(document).ready(function() {
 
-//       // let tagTitle = prompt("Please enter the title of your marker");
-//       $('.center').show();
-//       $(this).hide();
+  $('.block').on('click', onChange)
 
-//       $('#close').on('click', function () {
-//         $('.center').hide();
-//       })
+});
 
-//       $("#tag-information").submit(function(event) {
-//         event.preventDefault();
-//         newMarker.bindPopup(`<b>${$(this).find("#Tag-title").val()}</b><img width="150" height="150" src="${$(this).find("#Image-url-link").val()}">`);
-//         $('.center').hide();
-//       })
-
-//       $("#map2").css("cursor", "")
-//       markerButton = false;
-
-//     }
-//   }
-
-// });
-
-// const onChange = function () {
-//   return $(".titleTable").show()
-// };
+const onChange = function () {
+  return $("#titleTable").show()
+};
